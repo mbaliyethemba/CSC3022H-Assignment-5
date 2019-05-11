c6018801fbce09aed65598735b654de5441ca31d
Themba <SHNMBA004@uct.ac.za>
2019-05-11 20:29:55 +0200

    Added the operators for the function for the audio file

--- audio.h
+++ audio.h
@@ -53,5 +53,108 @@ namespace SHNMBA004{
 			audio(const std::string &filename, int &channel, int &rate) : noOfChannels(channel), sampleRateInHz(rate){
 				loadAudio(filename);
 			}
+			
+			audio() = default;
+			
+			//Destructor
+			~audio(){
+				noOfChannels = 0;
+				sampleRateInHz = 0;
+				noOfSamples = 0;
+				lenOfAudioSeconds = 0;
+				audioData.clear();
+				std::vector<BitType>().swap(audioData)
+			}
+			
+			//constructor
+			audio(int samples, int length, std::vector<BitType> v, int &channel, int &rate) : noOfChannels(channel), sampleRateInHz(rate), noOfSamples(samples), lenOfAudioSeconds(length), audioData(v) {
+			}
+			
+			//Copy constructor
+			audio(const audio &rhs) : noOfChannels(rhs.noOfChannels), sampleRateInHz(rhs.sampleRateInHz), noOfSamples(rhs.noOfSamples), lenOfAudioSeconds(rhs.lenOfAudioSeconds){
+			}
+			
+			//Move constructor
+			audio(audio &&rhs) : noOfChannels(rhs.noOfChannels), sampleRateInHz(rhs.sampleRateInHz), noOfSamples(rhs.noOfSamples), lenOfAudioSeconds(rhs.lenOfAudioSeconds){
+				rhs.noOfChannels = 0;
+				rhs.sampleRateInHz = 0;
+				rhs.noOfSamples = 0;
+				rhs.lenOfAudioSeconds = 0;
+				rhs.audioData.clear();
+			}
+			
+			//copy assignment operator
+			audio &operator=(const audio & rhs){
+				noOfChannels = rhs.noOfChannels;
+				sampleRateInHz = rhs.sampleRateInHz;
+				noOfSamples = rhs.noOfSamples;
+				lenOfAudioSeconds = rhs.lenOfAudioSeconds;
+				audioData = rhs.audioData;
+			}
+			
+			//move assignment operator
+			audio &operator=(audio && rhs){
+				noOfChannels = rhs.noOfChannels;
+				sampleRateInHz = rhs.sampleRateInHz;
+				noOfSamples = rhs.noOfSamples;
+				lenOfAudioSeconds = rhs.lenOfAudioSeconds;
+				audioData = rhs.audioData;
+				
+				rhs.noOfChannels = 0;
+				rhs.sampleRateInHz = 0;
+				rhs.noOfSamples = 0;
+				rhs.lenOfAudioSeconds = 0;
+				rhs.audioData.clear();
+			}
+		//for unit tests
+		int getNoOfChannels(){
+			return noOfChannels;
+		};
+		
+		int getSampleRate(){
+			return sampleRateInHz;
+		};
+		
+		int getNoOfSamples(){
+			return noOfSamples
+		};
+		
+		int getLenInSec(){
+			return lenOfAudioSeconds;
+		};
+		
+		bool checkIfAudioEmpty(){
+			return audioData.empty();
+		};
+		
+		//saves audio data into file
+		void saveAudio(const std::string & outfile){
+			std::string full = outfile + "_" + std::to_string(sampleRateInHz) + "_" + std::to_string(sizeof(BitType)*8) + "_mono.raw";
+			std::ofstream output(full, std::ios::binary | std::ios::out);
+			if(output.is_open()){
+				for(int i = 0; i < audioData.size(); ++i){
+					output.write(reinterpret_cast<const char *>(&audioData[i]), sizeof(BitType));
+				}
+			}
+			else{
+				std::cout << "Not able to write to file" << std::endl;
+				exit(0);
+			}
+		}
+		
+		//returns the size of file
+		long filesize(const std::string &filename){
+			std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+			return (long) in.tellg();
+		}
+		
+		//concatenate two audio data together
+		audio operator | (const audio & rhs){
+			audio con(*this);
+			con.audioDate.insert(con.audioData.end(), rhs.audioData.begin(), rhs.audioData.end());
+			con.lenOfAudioSeconds = con.lenOfAudioSeconds + rhs.lenOfAudioSeconds;
+			con.noOfSamples = con.noOfSamples + rhs.noOfSamples;
+			return con;
+		}		
 	}
 }
