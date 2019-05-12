20077ada8cf68be7126452e94a1c163fb71d639f
Themba <SHNMBA004@uct.ac.za>
2019-05-13 00:52:50 +0200

    Added the constructor test

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -2,3 +2,19 @@
 #include "catch.hpp"
 #include "audio.h"
 
+using namespace std;
+using namespace SHNMBA004;
+
+TEST_CASE("Test"){
+	SECTION("Constructor"){
+		string filename = "sample_input/beez18sec_44100_signed_8bit_mono.raw";
+		int channel = 1;
+		int srate = 44100;
+		audio<int8_t> audLoad = audio<int8_t>(filename,channel,rate);
+		REQUIRE((audLoad.getNoOfChannels() == channel));
+		REQUIRE((audLoad.getLenInSec() == 18));
+		REQUIRE((audLoad.getNoOfSamples() == 793800));
+		REQUIRE((audLoad.getSampleRate() == srate));
+	}
+	
+}
