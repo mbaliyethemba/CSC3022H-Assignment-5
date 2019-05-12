33a2c8b5f0eb2ca5258f0c9be0cb119bb4682f5d
Themba <SHNMBA004@uct.ac.za>
2019-05-13 01:23:12 +0200

    Added he copy and move operator assignements

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 using namespace SHNMBA004;
 
-TEST_CASE("Test"){
+TEST_CASE("Test : Special Members"){
 	SECTION("Constructor"){
 		string filename = "sample_input/beez18sec_44100_signed_8bit_mono.raw";
 		int channel = 1;
@@ -35,6 +35,22 @@ TEST_CASE("Test"){
 		audio<int8_t> copyAud(audLoad);
 		REQUIRE((audLoad == copyAud));
 		audio<int8_t> movedAud(move(audLoad));
+		REQUIRE((audLoad.getNoOfChannels() == 0));
+		REQUIRE((audLoad.getLenInSec() == 0));
+		REQUIRE((audLoad.getNoOfSamples() == 0));
+		REQUIRE((audLoad.getSampleRate() == 0));
+		REQUIRE(audLoad.checkIfAudioEmpty());
+	}
+	
+	SECTION("Copy/Move Assignment Operators"){
+		string filename = "sample_input/siren40sec_44100_signed_8bit_mono.raw";
+		int channel = 1;
+		int srate = 44100;
+		audio<int8_t> audLoad = audio<int8_t>(filename,channel,srate);
+		audio<int8_t> copyAud = audLoad;
+		REQUIRE((audLoad == copyAud));
+		audio<int8_t> movedAud = move(audLoad);
+		REQUIRE((audLoad == movedAud));
 		REQUIRE((audLoad.getNoOfChannels() == 0));
 		REQUIRE((audLoad.getLenInSec() == 0));
 		REQUIRE((audLoad.getNoOfSamples() == 0));
